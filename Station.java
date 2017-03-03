96789eb7abcc43da6946e1154ddb4598ebe533e7
StepW <stephane.wilhelm@ensg.eu>
2017-03-03 17:00:44 +0100

    Add files via upload

--- Station.java
+++ Station.java
@@ -3,12 +3,19 @@ package trajetMetro;
 public class Station {
 	
 	private String nom;
+	private double x;
+	private double y;
 	
-	public Station(String n){
+	public Station(String n,double x,double y){
 		nom = n;
+		this.x = x;
+		this.y = y;
 		
 	}
 	
+
+
+	//nous ne voulons pas de doublons dans la liste
 	@Override
 	public boolean equals(Object s){
 		if (this == s) return true;
@@ -28,11 +35,20 @@ public class Station {
 		return nom;
 	}
 
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
 	@Override
 	public String toString() {
-		return "Station [nom=" + nom + "]";
+		return "Station [nom=" + nom + ", x=" + x + ", y=" + y + "]";
 	}
-	
-	
+
+
+
 
 }
