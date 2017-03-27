cce889384225563de0dfce7406e65f207cca5992
StepW <stephane.wilhelm@ensg.eu>
2017-03-27 02:03:41 +0200

    Update Station.java

--- Station.java
+++ Station.java
@@ -1,39 +1,15 @@
 package trajetMetro;
 
 import java.awt.Color;
-import java.util.ArrayList;
 
 public class Station {
-	
 	
 	private int id;
 	private String nom;
 	private int x;
 	private int y;
-	private ArrayList<Integer> lid;
 	private Color c;
 	private boolean ferme;
-
-	
-	public Station(ArrayList<Integer> lid, int id,String n,int x,int y){
-		this.lid = lid;
-		lid.add(id);
-		nom = n;
-		this.x = x;
-		this.y = y;
-
-		
-	}
-	//test
-	
-	public Station(ArrayList<Integer> lid, int id,String n, Color c){
-		this.lid = lid;
-		lid.add(id);
-		this.id = id;
-		nom = n;
-		this.c = c;
-		
-	}
 	
 	public Station(int id,String n,int x,int y, Color c,boolean ferme){
 		this.id = id;
@@ -44,13 +20,7 @@ public class Station {
 		this.ferme = ferme;
 		
 	}
-	
-	public void addId(int id){
-		this.getLid().add(id);
-	}
 
-
-	//nous ne voulons pas de doublons dans la liste
 	@Override
 	public boolean equals(Object s){
 		if (this == s) return true;
@@ -65,20 +35,7 @@ public class Station {
         result = getId();
         return result;
     }
-	
-	
 
-
-	public ArrayList<Integer> getLid() {
-		return lid;
-	}
-
-	public void setLid(ArrayList<Integer> lid) {
-		this.lid = lid;
-	}
-
-	
-	
 	public String getNom() {
 		return nom;
 	}
@@ -99,10 +56,6 @@ public class Station {
 		this.y = y;
 	}
 	
-	
-	
-	
-
 	public int getId() {
 		return id;
 	}
@@ -110,8 +63,6 @@ public class Station {
 	public void setId(int id) {
 		this.id = id;
 	}
-	
-	
 
 	public Color getC() {
 		return c;
@@ -120,7 +71,6 @@ public class Station {
 	public void setC(Color c) {
 		this.c = c;
 	}
-	
 
 	public boolean isFerme() {
 		return ferme;
@@ -134,12 +84,5 @@ public class Station {
 	public String toString() {
 		return "Station [id=" + id + ", nom=" + nom + ", x=" + x + ", y=" + y + ", c=" + c + ", ferme=" + ferme + "]";
 	}
-
-
-
-
-
-
-
 
 }
